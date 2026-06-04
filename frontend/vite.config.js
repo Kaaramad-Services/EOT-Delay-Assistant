7468a5f4b061da69141bef66a7cd300c8e55a40a
Muhammad Suleman <kaaramad@workmail.com>
2026-06-04 10:39:44 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/eot-delay-assistant/',
+  base: '/Eot-Delay-Assistant/',
   server: {
     proxy: {
       '/api': {
